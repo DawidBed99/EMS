38ee3af7281e73b5c914a1dd29cadd3765037820
Dawid Bednarz <160144@stud.prz.edu.pl>
2023-12-08 17:41:30 +0100

    deleted files

--- frontend/src/components/EditData.jsx
+++ frontend/src/components/EditData.jsx
@@ -13,7 +13,7 @@ import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import dayjs from "dayjs";
+
 
 const AddEmployee = () => {
   const params = useParams();
