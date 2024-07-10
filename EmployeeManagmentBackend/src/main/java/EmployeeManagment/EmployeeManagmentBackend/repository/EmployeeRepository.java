03a719846edf30b6c1e9a04ef22c3c3c3f571ea9
DawidBed99 <128499430+DawidBed99@users.noreply.github.com>
2024-07-10 16:46:14 +0200

    Delete EmployeeManagmentBackend directory

--- EmployeeManagmentBackend/src/main/java/EmployeeManagment/EmployeeManagmentBackend/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package EmployeeManagment.EmployeeManagmentBackend.repository;
-import EmployeeManagment.EmployeeManagmentBackend.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
